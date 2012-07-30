1d56acdecf55a7e55b55c464862a0fb15471feb1
Chris Doyle <archslide@gmail.com>
2012-07-30 18:16:39 -0500

    bump to v2

--- lib/transformable.rb
+++ lib/transformable.rb
@@ -12,9 +12,12 @@ module Transformable
 
       define_method "#{attr}=" do |value|
 	new_value = yield(value)
+	puts new_value
 	instance_variable_set("@#{attr}", new_value)
-	write_attribute(attr, new_value) if respond_to? :write_attribute
-	new_value
+	begin
+	  write_attribute(attr, new_value)
+	rescue NoMethodError
+	end
       end
     end
   end
