7e88c34768812f4179e60558161ed9049d69a953
Chris Doyle <archslide@gmail.com>
2012-07-31 09:58:52 -0500

    only give nils to the block if explicitly asked

--- lib/transformable.rb
+++ lib/transformable.rb
@@ -8,10 +8,16 @@ module Transformable
 
 
   module ClassMethods
-    def clean(attr, &blk)
+    def clean(attr, options={}, &blk)
+
+      skip_nil = options.fetch(:skip_nil) { true }
 
       define_method "#{attr}=" do |value|
-	new_value = yield(value)
+	if value.nil? and skip_nil
+	  new_value = value
+	else
+	  new_value = yield(value)
+	end
 	instance_variable_set("@#{attr}", new_value)
         write_attribute(attr, new_value) if defined? ActiveRecord and defined? ActiveRecord::Base and is_a? ActiveRecord::Base
       end
--- spec/transformable_spec.rb
+++ spec/transformable_spec.rb
@@ -6,10 +6,45 @@ class Foo
   clean(:title) { |v| v.gsub(/\s/, "")}
 end
 
+class Bar
+  include Transformable
+  attr_accessor :title
+  clean(:title, :skip_nil => false) {|v| v.nil? ? "bar" : v }
+end
+
+class NoNils
+  include Transformable
+  attr_accessor :title
+  clean(:title) {|v| "bar" }
+end
+
 describe Transformable do
   it "used the provided block to filter attributes" do
     f = Foo.new
     f.title = "bar bar"
     f.title.should == "barbar"
+  end
+
+  context "when :skip_nil is not set (aka :skip_nil => true)" do
+    it "doesn't let nil values through to the block" do
+      n = NoNils.new
+      n.title = nil
+      n.title.should be_nil
+    end
+  end
+
+
+  context "when :skip_nil is set to false" do
+    it "lets nil values through" do
+      b = Bar.new
+      b.title = nil
+      b.title.should == "bar"
+    end
+
+    it "lets non-nil values through" do
+      b = Bar.new
+      b.title = "foobar"
+      b.title.should == "foobar"
+    end
   end
 end
